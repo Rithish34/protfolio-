5ef15d761ee227f7683e1097f80155db94aa6503
Rithish kumar <150705896+Rithish34@users.noreply.github.com>
2024-06-10 17:18:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".text",{
+    strings:["Frontend Developer", "Web developer", "python developer"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
+function redirectToWebsite() {
+    window.location.href = "https://www.google.com";}
